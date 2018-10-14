7e707283e6d2412f7c29bade3d73bf920b06ec84
Galza1991 <garethrhawkins@gmail.com>
2018-10-14 20:28:22 +0100

    Add project files.

---
+++ TabularTextSorter/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TabularTextSorter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
